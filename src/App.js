1bdffb5a62a6868f983fff80da3ddd2d1392e7f0
onodera.manabu <onodera.manabu@isid.co.jp>
2019-03-25 00:58:00 +0900

    learn prop-types

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React  from 'react';
+import PropTypes from 'prop-types';
 
 const App = () => { //関数コンポーネント
   const profiles = [
-    {name:"Taro", age: 10},
-    {name:"Hanako", age: 5},
-    {name:"NoName"}
-
+    {name:"Taro",   age:10},
+    {name:"Hanako", age:5},
+    {name:"NoName", age:3}
   ]
   return(
   <div>
@@ -21,8 +21,8 @@ const User = (props) => { //関数コンポーネント
   return <div>Hi, I am {props.name}, and {props.age} years old!</div>
 }
 
-User.defaultProps = {
-  age:1
+User.propTypes = {
+  name: PropTypes.string,
+  age : PropTypes.number.isRequired
 }
-
 export default App;
